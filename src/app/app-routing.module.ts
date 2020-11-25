50283ade57bc6c8dab887b0bbd4631b4e83e37f8
tavinogradova92 <tavinogradova92@gmail.com>
2020-11-25 14:16:28 +0100

    Routers added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { TrainerPageComponent } from './components/trainer-page/trainer-page.component';
 
 const routes: Routes = [
-  
+  {
+    path: 'login',
+    component: LandingPageComponent
+  },
+  {
+    path: 'dashboard',
+    component: TrainerPageComponent
+  },
+  {
+    path: 'pokemons',
+    component: PokemonsComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/login'
+  }
 ];
 
 @NgModule({

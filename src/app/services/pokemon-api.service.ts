94cbd991d47b7bf4c32d59c1cfb58a667540b2c8
tavinogradova92 <tavinogradova92@gmail.com>
2020-11-26 13:10:54 +0100

    Pokemon fetch service done

--- src/app/services/pokemon-api.service.ts
+++ src/app/services/pokemon-api.service.ts
@@ -1,16 +1,63 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Pokemon } from '../models/pokemon';
+import { catchError, map, tap } from 'rxjs/operators';
+import { MessageService } from '../services/message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonAPIService {
 
-  constructor(private http: HttpClient) { }
+  private apiUrl: string = 'https://pokeapi.co/api/v2/pokemon';
+  private apiLimit: string = '?limit=200';
 
-  public catchPokemons(pockemon: Object): Promise<any> {
-    return this.http.post('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200', {
-      pockemon
-    }).toPromise();
+  constructor(private http: HttpClient, private messageService: MessageService) {
   }
+
+  public fetchPokemons(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>(`${this.apiUrl}${this.apiLimit}`)
+      .pipe(
+        map((res: any) => {
+          return res.results.map((pokemon: Pokemon) => ({
+            ...pokemon,
+            ...this.getIdAndImage(pokemon.url)
+          }))
+        }),
+        tap(_ => this.log('fetched pokemons')),
+        catchError(this.handleError<Pokemon[]>('fetchPokemons', []))
+      );
+  }
+
+  private getIdAndImage(url: string): any {
+    const id = url.split( '/' ).filter( Boolean ).pop();
+    return {id, 
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ id }.png`};
+  }
+
+  public fetchPokemon(id: number): Observable<Pokemon> {
+    return this.http.get<Pokemon>(`${this.apiUrl}/${id}`)
+      .pipe(
+        tap(_ => this.log(`fetched pokemon id=${id}`)),
+        catchError(this.handleError<Pokemon>(`fetchPokemon id=${id}`))
+      );
+  }
+
+  // Handle Http operation that failed.
+
+  private log(message: string) {
+    this.messageService.add(`PokemonService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); 
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
 }

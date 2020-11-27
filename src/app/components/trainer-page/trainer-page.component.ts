60fccc76a67a25cf4bc9a7787520568c832e0cbd
tavinogradova92 <tavinogradova92@gmail.com>
2020-11-27 14:56:00 +0100

    Log Out button added

--- src/app/components/trainer-page/trainer-page.component.ts
+++ src/app/components/trainer-page/trainer-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthServiceService } from '../../services/auth-service.service';
+import { PokemonCollectorService } from '../../services/pokemon-collector.service';
 
 @Component({
   selector: 'app-trainer-page',
@@ -11,10 +14,23 @@ export class TrainerPageComponent implements OnInit {
 
   public trainerName = JSON.parse(localStorage.getItem('username')); 
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthServiceService,
+    private pokemonCollector: PokemonCollectorService
+  ) { }
 
   ngOnInit(): void {
     this.pokemonCollection = JSON.parse(localStorage.getItem('collectedPokemons'));
+  }
+
+  logout(): void {
+    if (confirm("Are you sure you'd like to log out? It will delete all your data.")) {
+      this.authService.isLogedIn = false;
+      this.pokemonCollector.collectedPokemons = [];
+      localStorage.clear();
+      this.router.navigateByUrl('/login');
+    } 
   }
 
 }

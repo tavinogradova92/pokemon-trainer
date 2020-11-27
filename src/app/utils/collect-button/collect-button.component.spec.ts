0a645cafee3aa4f5921db4ae0a22cc30ca6b1989
tavinogradova92 <tavinogradova92@gmail.com>
2020-11-27 13:46:55 +0100

    Collect button created as a separate reusable component

---
+++ src/app/utils/collect-button/collect-button.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CollectButtonComponent } from './collect-button.component';
+
+describe('CollectButtonComponent', () => {
+  let component: CollectButtonComponent;
+  let fixture: ComponentFixture<CollectButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CollectButtonComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CollectButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/utils/collect-button/collect-button.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonCollectorService } from '../../services/pokemon-collector.service';
+
+@Component({
+  selector: 'app-collect-button',
+  templateUrl: './collect-button.component.html',
+  styleUrls: ['./collect-button.component.scss']
+})
+export class CollectButtonComponent implements OnInit {
+
+  @Input() pokemon: Pokemon;
+
+  constructor(private pokemonCollectorService: PokemonCollectorService) { }
+
+  ngOnInit(): void {
+  }
+
+  collect(id: number) {
+    this.pokemonCollectorService.collectPokemon(id);
+  }
+
+}
